bb4ca42a04ab15d4c9c0970e0bdcca7a2a83bbc5
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-06-06 15:26:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+let mapping = {
+  "2" : "abc",
+  "3" : "def",
+  "4" : "ghi",
+  "5" : "jkl" ,
+  "6" : "mno" , 
+  "7" : "pqrs" , 
+  "8" : "tuv"  ,
+  "9" : "wxyz"
 }
 
-module.exports = letterCombinations;
+let results = [] ;
+let searchString = "23" ;
+
+function solve(index, calculatedString) {
+  if(index >= searchString.length) {
+    results.push(calculatedString) ;
+    return ;
+  }
+  let curString = mapping[searchString[index]] ;// mapping[searchString[0]] => mapping["2"] => "abc"
+  for(let i = 0 ; i < curString.length ; i++ ) {
+    solve(index + 1, calculatedString + curString[i]) ; // index=1, calculatedString = ("" + curString)=> abc;
+  }
+}
+
+solve(0, "") ;
+console.log(results) ;
+
+module.exports = solve;
